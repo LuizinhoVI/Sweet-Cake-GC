97573042199d297fc175c25d0887ad86b0cdef14
LUIZ VIEIRA <beat.luiz23@hotmail.com>
2021-08-17 17:33:16 -0300

    att

--- valores.js
+++ valores.js
@@ -1,5 +1,6 @@
 var total_compras = 0;
 
+
 function meu_valor() {
 
     var valor_total = 0;
@@ -28,13 +29,15 @@ function meu_valor() {
 
 
 function escolher_prato() {
+
     //variaveis dos pratos
     var tipo = document.getElementById("tipo_prato").value;
     var doce = document.getElementById("doce");
     var salgado = document.getElementById("salgado");
-    //   var salgado_parte = document.getElementById("salgado_parte");
+    var salgado_parte = document.getElementById("salgado_parte");
     var bolo = document.getElementById("bolo");
     var bolo_parte = document.getElementById("bolo_parte");
+    var bolo_mini = document.getElementById("bolo_mini");
     var gourmet = document.getElementById("gourmet");
     var copo_caldas = document.getElementById("copo_caldas");
     var copo_massa = document.getElementById("copo_massa");
@@ -50,39 +53,88 @@ function escolher_prato() {
     } else { doce.style.display = "none" }
     if (tipo == "Salgado") {
         salgado.style.display = "block"
+        if (salgado.value == "1") {
+            salgado_parte.style.display = "block"
+        }
+        if (salgado_parte.value == "1") {
+            total_compras = 27;
+            compras.value = "R$ 27,00";
 
-    } else { salgado.style.display = "none" }
+        }
+        if (salgado_parte.value == "2") {
+            total_compras = 4.50;
+            compras.value = "R$ 4,50";
+
+        }
+        if (salgado_parte.value == 0) {
+            compras.value = "R$ 0,00";
+            total_compras = 0;
+        }
+    } else {
+        salgado.style.display = "none"
+        salgado.value = 0;
+        salgado_parte.style.display = "none"
+        salgado_parte = 0;
+        total_compras = 0;
+        compras.value = "R$ 0,00";
+    }
 
 
     // vai mostrar as opções  do bolo
     if (tipo == "Bolo") {
         bolo.style.display = "block"
-        if (bolo.value == "2") {
-            bolo_parte.style.display = "block"
+
+
+
+        if (bolo.value == "0") {
+            bolo_parte.style.display = "none"
+            bolo_mini.style.display = "none"
         }
+        if (bolo_mini.value == "1") {
+            total_compras = 15;
+            compras.value = "R$ 15,00";
+
+        }
+        if (bolo.value == "1") {
+            bolo_mini.style.display = "block"
+            bolo_parte.style.display = "none"
+            total_compras = 0;
+            compras.value = "R$ 00,00";
+        }
+
+        if (bolo.value == "2") {
+            bolo_mini.style.display = "none"
+            bolo_parte.style.display = "block"
+            total_compras = 0;
+            compras.value = "R$ 00,00";
+
+        }
+
+
+        if (bolo_parte.value == "1") {
+            total_compras = 70;
+            compras.value = "R$ 70,00";
+        }
+
         if (bolo_parte.value == "2") {
             total_compras = 7;
             compras.value = "R$ 7,00";
 
         }
-        if (bolo_parte.value == "1") {
-            total_compras = 70;
-            compras.value = "R$ 70,00";
 
-        }
-        if (bolo_parte.value == 0) {
-            compras.value = "R$ 0,00";
-            total_compras = 0;
-        }
+
 
     } else {
         bolo.style.display = "none"
-        bolo.value = 0;
         bolo_parte.style.display = "none"
+        bolo_mini.style.display = "none"
+        bolo.value = 0;
+        bolo_mini.value = 0;
         bolo_parte.value = 0;
         compras.value = "R$ 0,00";
         total_compras = 0;
     }
+
 
     console.log(total_compras)
 
@@ -100,8 +152,8 @@ function escolher_prato() {
         copo_cobertura.style.display = "none"
         copo_caldas.style.display = "none"
     }
-
 }
+
 
 
 
