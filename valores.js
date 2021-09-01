4f53a6e6eb22749ccd952c78ac5ece2b227a1b16
luizinhovi <beat.luiz23@hotmail.com>
2021-09-01 12:31:08 -0300

    atualização

--- valores.js
+++ valores.js
@@ -461,26 +461,6 @@
 
 
 
-
-
-
-
- function gera_id() {
-     var size = 10 //tamanho do ID e armazena na variável
-     var randomized = Math.ceil(Math.random() * Math.pow(10, size)); //Cria um número aleatório do tamanho definido em size.
-     var digito = Math.ceil(Math.log(randomized)); //Cria o dígito verificador inicial
-     while (digito > 10) { //Pega o digito inicial e vai refinando até ele ficar menor que dez
-         digito = Math.ceil(Math.log(digito));
-     }
-     var id = randomized + '-' + digito; //Cria a ID
-     alert(id);
- }
-
-
-
-
-
-
  // função para limitar os caracteres da Saida de entrada dos valores 
  // no object tem que colocar o this
  function numero_max(object) {
